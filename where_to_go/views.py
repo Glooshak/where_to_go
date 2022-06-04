1f8031b0753cf78502548deaa8632b137da523d6
Dmitrii <glooshak@gmail.com>
2022-06-04 21:36:44 +0300

    Move data from template to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -5,9 +5,40 @@ from django.http import (
 from django.template import loader
 
 
+geo_json = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.62, 55.793676]
+          },
+          "properties": {
+            "title": "«Легенды Москвы",
+            "placeId": "moscow_legends",
+            "detailsUrl": "static/places/moscow_legends.json"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.64, 55.753676]
+          },
+          "properties": {
+            "title": "Крыши24.рф",
+            "placeId": "roofs24",
+            "detailsUrl": "static/places/roofs24.json"
+          }
+        }
+      ]
+    }
+
+
 def show_main_page(
         request: HttpRequest,
 ) -> HttpResponse:
     return HttpResponse(
-        loader.get_template('main.html').render({}, request)
+        loader.get_template('index.html').render({'geo_json': geo_json}, request)
     )
