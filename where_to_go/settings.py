45e215619e79e9ac48cca7796a9c7a9964924494
Dmitrii <glooshak@gmail.com>
2022-06-04 20:10:18 +0300

    Setup media files serving

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -124,6 +124,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / 'uploaded_media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
