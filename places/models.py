b3f786d864fce5c46225bbb7a8e6d35f533c9442
Dmitrii <glooshak@gmail.com>
2022-06-05 13:27:18 +0300

    Fronted takes data from db

--- places/models.py
+++ places/models.py
@@ -8,20 +8,43 @@ class Place(models.Model):
         blank=False,
         db_index=False,
     )
+
     description_short = models.CharField(
         max_length=100,
         blank=False,
     )
+
     description_long = models.TextField(
         blank=False,
     )
+
     coordinates = models.JSONField(
         blank=False,
         unique=True,
     )
 
+    place_id = models.CharField(
+        max_length=15,
+        blank=False,
+    )
+
+    @property
+    def lng(self) -> float:
+        return float(self.coordinates['lng'])
+
+    @property
+    def lat(self) -> float:
+        return float(self.coordinates['lat'])
+
+    def __str__(self) -> str:
+        return self.title
+
 
 class Image(models.Model):
+
+    place = models.ForeignKey(
+        Place, on_delete=models.CASCADE
+    )
 
     title = models.CharField(
         max_length=50,
@@ -36,3 +59,6 @@ class Image(models.Model):
     picture = models.ImageField(
         blank=False,
     )
+
+    def __str__(self) -> str:
+        return self.title
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -4,41 +4,36 @@ from django.http import (
 )
 from django.template import loader
 
-
-geo_json = {
-      "type": "FeatureCollection",
-      "features": [
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.62, 55.793676]
-          },
-          "properties": {
-            "title": "«Легенды Москвы",
-            "placeId": "moscow_legends",
-            "detailsUrl": "static/places/moscow_legends.json"
-          }
-        },
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.64, 55.753676]
-          },
-          "properties": {
-            "title": "Крыши24.рф",
-            "placeId": "roofs24",
-            "detailsUrl": "static/places/roofs24.json"
-          }
-        }
-      ]
-    }
+from places.models import Place
 
 
 def show_main_page(
         request: HttpRequest,
 ) -> HttpResponse:
+    geo_json = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    detail_url = {
+        'moscow_legends': 'static/places/moscow_legends.json',
+        'roofs24': 'static/places/roofs24.json',
+    }
+    for place in Place.objects.all():
+        place_id = place.place_id
+        geo_json['features'].append(
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [place.lng, place.lat]
+                },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place_id,
+                    "detailsUrl": detail_url[place_id]
+                }
+            }
+        )
     return HttpResponse(
         loader.get_template('index.html').render({'geo_json': geo_json}, request)
     )
