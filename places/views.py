bb85eff6ab256f6f159e5fd9f7b4d08c96ec1754
Dmitrii <glooshak@gmail.com>
2022-06-05 14:11:37 +0300

    Returning a place description in a response

--- places/views.py
+++ places/views.py
@@ -1,6 +1,6 @@
 from django.http import (
     HttpRequest,
-    HttpResponse,
+    JsonResponse,
 )
 from django.shortcuts import get_object_or_404
 
@@ -10,7 +10,19 @@ from .models import Place
 def fetch_place_title(
         request: HttpRequest,
         place_id: int,
-) -> HttpResponse:
+) -> JsonResponse:
 
     place = get_object_or_404(Place, pk=place_id)
-    return HttpResponse(place.title)
+    return JsonResponse(
+        {
+            'title': place.title,
+            'description_short': place.description_short,
+            'description_long': place.description_long,
+            'coordinates': place.coordinates,
+            'imgs': [
+                image.picture.url for image in place.image_set.all()
+            ],
+        },
+        safe=False,
+        json_dumps_params={'ensure_ascii': False, 'indent': 4},
+    )
