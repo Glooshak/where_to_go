e8931eef9575f5661a9b7ce86d2b020d5e4527e9
Dmitrii <glooshak@gmail.com>
2022-06-05 15:40:30 +0300

    Image previes in amdin panel

--- places/admin.py
+++ places/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import (
     Image,
@@ -8,6 +9,21 @@ from .models import (
 
 class ImageInLine(admin.TabularInline):
     model = Image
+    readonly_fields = 'get_preview',
+
+    def get_preview(self: 'ImageInLine', image: Image) -> str:
+        picture = image.picture
+        url, height, width = picture.url, picture.height, picture.width
+        if height > 200:
+            old_aspect_ratio = height / width
+            height = 200
+            width = height / old_aspect_ratio
+            new_aspect_ratio = height / width
+            assert new_aspect_ratio == old_aspect_ratio, 'Aspect ration was not preserved!'
+
+        return format_html(
+            f'<img src="{url}" width="{width}" height={height} />'
+        )
 
 
 @admin.register(Place)
